f865d9a88a62fab59a57cca32a6e208aabebab5f
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-03-03 19:15:33 +0100

    Part 1 done.

--- drafts.py
+++ drafts.py
@@ -1,4 +1,11 @@
 
 
-tupla_anidado_listas = ('a', 'b', ['c', 'd'], ['e', 'f'])
-print(tupla_anidado_listas)
+# tupla_anidado_listas = ('a', 'b', ['c', 'd'], ['e', 'f'])
+# print(tupla_anidado_listas)
+
+
+def lambdaa(name):
+    name = lambda name: f'Hi, {name}'
+    return lambdaa
+
+print(lambdaa('Jordan'))
