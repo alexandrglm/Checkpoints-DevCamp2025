be1cd952ed849b776eec54e2f7b0adab8e211f6b
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-03-03 10:53:00 +0100

    Checkpoint 5 Started

---
+++ 05/Checkpoint-05.py
@@ -0,0 +1,33 @@
+# Checkpoint 05
+
+# 1. Cree un bucle For de Python.
+
+for num in range(1,11):
+    print(f'Esta es la lína num. {num}.')
+
+# 2. Cree una función de Python llamada suma que tome 3 argumentos y devuelva la suma de los 3.
+
+def suma(a, b, c):
+    
+    return a + b + c
+
+print(suma(12,14,16))
+
+
+# 3. Cree una función lambda con la misma funcionalidad que la función de suma que acaba de crear.
+
+
+
+# 4. -Utilizando la siguiente lista y variable, determine si el valor de la variable coincide o no con un valor de la lista. *Sugerencia, si es necesario, utilice un bucle for in y el operador in.
+
+
+nombre = 'Enrique'
+
+lista_nombre = ['Jessica', 'Paul', 'George', 'Henry', 'Adán']
+
+
+if nombre in lista_nombre == True:
+    print(f'El valor {nombre} sí está en la lista')
+else:
+    print(f'El valor {nombre} no está en la lista')
+
